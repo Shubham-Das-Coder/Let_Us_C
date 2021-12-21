e5beccac8715882476af08adb19915be30d0168b
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-21 23:15:24 +0530

    Add files via upload

---
+++ Let_Us_C_Structures_8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    struct book
+    {
+        char name[25];
+        char author[25];
+        int callno;
+    };
+    struct book b1 = {"Let Us C", "YPK", 101};
+    struct book *ptr;
+    ptr = &b1;
+    printf("%s  %s  %d\n", b1.name, b1.author, b1.callno);
+    printf("%s  %s  %d\n", ptr->name, ptr->author, ptr->callno);
+    return 0;
+}
