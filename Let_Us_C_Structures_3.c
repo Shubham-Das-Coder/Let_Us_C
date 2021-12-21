ecd760d92d890193acd9f1a883e41121da9aad10
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-21 19:38:09 +0530

    Add files via upload

---
+++ Let_Us_C_Structures_3.c
@@ -0,0 +1,38 @@
+// Usage of an array of structures
+#include <stdio.h>
+
+void linkfloat();
+
+int main()
+{
+    struct book
+    {
+        char name;
+        float price;
+        int pages;
+    };
+    struct book b[100];
+
+    int i;
+
+    for (i = 0; i < 100; i++)
+    {
+        printf("Enter name, price and pages\n");
+        fflush(stdin);
+        scanf("%c%f%d", &b[i].name, &b[i].price, &b[i].pages);
+    }
+
+    for (i = 0; i < 100; i++)
+    {
+        printf("%c%f%d", &b[i].name, &b[i].price, &b[i].pages);
+    }
+
+    return 0;
+}
+
+void linkfloat()
+{
+    float a = 0, *b;
+    b = &a; // Cause emulator to be linked
+    a = *b; // Suppress the warning - variable not used
+}
