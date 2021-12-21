5a822a8e302c0e7e0da6d1c545809e1217022521
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-21 22:47:13 +0530

    Add files via upload

---
+++ Let_Us_C_Structures_6.c
@@ -0,0 +1,24 @@
+// Passing individual structures elements
+#include <stdio.h>
+
+void display(char *, char *, int);
+
+int main()
+{
+    struct book
+    {
+        char name[25];
+        char author[25];
+        int callno;
+    };
+    struct book b1 = {"Let Us C", "YPK", 101};
+
+    display(b1.name, b1.author, b1.callno);
+
+    return 0;
+}
+
+void display(char *s, char *t, int n)
+{
+    printf("%s   %s   %d\n", s, t, n);
+}
