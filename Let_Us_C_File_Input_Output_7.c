805fdd4e70f1fa056791379b2d4aca5ffca7df7f
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-23 22:41:18 +0530

    Add files via upload

---
+++ Let_Us_C_File_Input_Output_7.c
@@ -0,0 +1,36 @@
+// Writes records to a file ussing structure
+#include <stdio.h>
+int main()
+{
+    FILE *fp;
+    char another = 'Y';
+    struct emp
+    {
+        char name[40];
+        int age;
+        float bs;
+    };
+    struct emp e;
+
+    fp = fopen("EMPLOYEE.DAT", "w");
+
+    if (fp == NULL)
+    {
+        puts("Cannot open file");
+        exit(1);
+    }
+
+    while (another == 'Y')
+    {
+        printf("Enter name, age and basic salary : ");
+        scanf("%s%d%f", e.name, &e.age, &e.bs);
+        fprintf(fp, "%s%d%f\n", e.name, e.age, e.bs);
+
+        printf("Add another record(Y/N) : ");
+        fflush(stdin);
+        another = getch();
+    }
+
+    fclose(stdin);
+    return 0;
+}
